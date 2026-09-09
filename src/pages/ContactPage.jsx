b2b724ebb048021bf6c64c427a081cad1f510d1c
Priyanshi9891 <priyumaurya02@gmail.com>
2026-09-09 15:24:10 +0530

    change3

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -2110,7 +2110,7 @@ export default function ContactPage() {
               ============================================= */}
 
               <Link
-                to="//book-services"
+                to="/book-services"
                 className="
                   group
                   inline-flex
